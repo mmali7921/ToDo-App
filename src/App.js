d66c82ec6c85a74f45ba20de2195f9644b342471
Muhammed Ali <mmali2004@gmail.com>
2025-01-16 00:22:58 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ function App() {
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,{id:Date.now(),text: toDo,status: false}])} className="fas fa-plus"></i>
+        <i onClick={()=>{
+          setToDos([...toDos,{id:Date.now(),text: toDo,status: false}]);
+          setToDo('')
+          }} className="fas fa-plus"></i>
       </div>
       <div className="todos">
          {
@@ -25,21 +28,24 @@ function App() {
          <div className="todo">
            <div className="left">
             <input onChange={(e)=>{
-              console.log(e.target.checked)
-              console.log(obj)
+    
               setToDos(toDos.filter(obj2=>{
                 if(obj2.id===obj.id){
                   obj2.status=e.target.checked
                 }
                 return obj2
               }))
-              console.log(obj)
+             
             }}
             value={obj.status} type="checkbox" name="" id="" />
             <p>{obj.text}</p>
            </div>
            <div className="right">
-            <i className="fas fa-times"></i>
+            <i onClick={(e)=>{
+              setToDos(toDos.filter(obj2=>{
+                return obj2.id!==obj.id
+              }))
+            }} className="fas fa-times"></i>
            </div>
           </div>)
           })}
