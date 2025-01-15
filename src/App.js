4f3b749f0357d1a53d8f036d129a4b2100d0f61f
Muhammed Ali <mmali2004@gmail.com>
2025-01-15 20:25:51 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -11,21 +11,32 @@ function App() {
       </div>
       <div className="subHeading">
         <br />
-        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        <h2>Whoop, it's {new Date().toLocaleDateString('en-US', { weekday: 'long' })} 🌝 ☕ </h2>
       </div>
       <div className="input">
         <input value={toDo} onChange={(e)=>setToDo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
-        <i onClick={()=>setToDos([...toDos,toDo])} className="fas fa-plus"></i>
+        <i onClick={()=>setToDos([...toDos,{id:Date.now(),text: toDo,status: false}])} className="fas fa-plus"></i>
       </div>
       <div className="todos">
          {
-          toDos.map((value)=>{
+          toDos.map((obj)=>{
 
           return(
          <div className="todo">
            <div className="left">
-            <input type="checkbox" name="" id="" />
-            <p>{value}</p>
+            <input onChange={(e)=>{
+              console.log(e.target.checked)
+              console.log(obj)
+              setToDos(toDos.filter(obj2=>{
+                if(obj2.id===obj.id){
+                  obj2.status=e.target.checked
+                }
+                return obj2
+              }))
+              console.log(obj)
+            }}
+            value={obj.status} type="checkbox" name="" id="" />
+            <p>{obj.text}</p>
            </div>
            <div className="right">
             <i className="fas fa-times"></i>
